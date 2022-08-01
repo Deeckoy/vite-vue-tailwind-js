1b630c3f48c3006166f09ec7c1fbc8ad7a04b608
Edward Haussauer <edhaussauer@gmail.com>
2022-08-01 20:04:25 +0300

    small fixes

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import "virtual:windi.css";
 import App from "@/App.vue";
 import router from "@/router/index";
 
-import en from "./locales/en.yml"
+import en from "@/locales/en.yml"
 import ru from "@/locales/ru.yml"
 
 const i18n = createI18n({
