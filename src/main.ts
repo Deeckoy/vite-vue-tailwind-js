d81aaef79a754728d7cf4147af610f44be27dced
Edward Haussauer <edhaussauer@gmail.com>
2022-09-11 09:54:52 +0300

    remove windicss

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { createI18n } from "vue-i18n";
 
-import "virtual:windi.css";
-
 import App from "@/App.vue";
 import router from "@/router/index";
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,21 @@
-import { defineConfig } from "vite"
-import { resolve, dirname } from "node:path"
-import { fileURLToPath, URL } from "url";
+import { defineConfig } from 'vite'
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath, URL } from 'url'
 
-import vue from "@vitejs/plugin-vue";
-import WindiCSS from "vite-plugin-windicss";
-import { vueI18n } from "@intlify/vite-plugin-vue-i18n"
+import vue from '@vitejs/plugin-vue'
+import { vueI18n } from '@intlify/vite-plugin-vue-i18n'
 
 export default defineConfig({
 	plugins: [
 		vue(),
-		WindiCSS({
-			preflight: true,
-			sortUtilities: true,
-		}),
 		vueI18n({
-			include: resolve(dirname(fileURLToPath(import.meta.url)), "./src/locales/**"),
-		})
+			include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),
+		}),
 	],
 
 	resolve: {
 		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
 	},
 
@@ -31,4 +26,4 @@ export default defineConfig({
 			},
 		},
 	},
-});
+})
